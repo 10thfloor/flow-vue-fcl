70f9c1cc6a16dfb41e38af06881b34dbdd76d13d
Mackenzie <remember.mackenzie@gmail.com>
2021-08-19 10:15:12 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img
+    alt="Flow logo"
+    width="200"
+    src="https://cryptologos.cc/logos/flow-flow-logo.png?v=010"
+  />
+  <HelloFlow msg="Hello Flow" />
+</template>
+
+<script>
+import HelloFlowVue from "./components/HelloFlow.vue";
+export default {
+  name: "App",
+  components: {
+    HelloFlow: HelloFlowVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
